44f0c3056967ee1c4ed76e18ac0a3012c30c7b2d
keithpops <keith.poplawski@gmail.com>
2016-02-12 22:36:46 -0500

    refactors app component

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -2,14 +2,8 @@
 import React, { Component } from 'react';
 
 // COMPONENT
-import Canvas from '../Canvas/Canvas';
-import Home from '../Home/Home';
-import PlayerSelection from '../Home/PlayerSelection';
+import Game from '../Game/Game';
 import HtmlHeaderTags from '../Document/HtmlHeaderTags';
-
-// FLUX
-import AppStore from '../../stores/AppStore';
-import AppActions from '../../actions/AppActions';
 
 if (process.env.BROWSER) {
   require('./_App.scss');
@@ -18,74 +12,29 @@ if (process.env.BROWSER) {
 }
 
 export default class App extends Component {
-  //
-  // static getStores(props) {
-  //   return [AppStore];
-  // }
-  //
-  // static getPropsFromStores(props) {
-  //   return AppStore.getState();
-  // }
 
-  // constructor() {
-  //   super();
-  //
-  //   this._renderApp = this._renderApp.bind(this);
-  //   this.handleButtonTap = this.handleButtonTap.bind(this);
-  // }
-  //
-  //
-  // componentDidMount() {
-  //
-  //   if (!('webkitSpeechRecognition' in window)) {
-  //     console.log('you don\'t have speech recog');
-  //   } else {
-  //     AppActions.loading(false);
-  //   }
-  // }
-  //
-  // handleButtonTap() {
-  //   this.setState({selecting: true});
-  // }
-  //
-  // _renderLoading() {
-  //   return (
-  //     <p>Loading...</p>
-  //   );
-  // }
+  constructor() {
+    super();
 
-  // _renderApp(selecting) {
-  //   let component = <Home onButtonTap={this.handleButtonTap} />
-  //
-  //   if (selecting) {
-  //     component = <PlayerSelection />
-  //   }
-  //
-  //   return (
-  //     <div className='main-content'>
-  //       {component}
-  //     </div>
-  //   );
-  // }
+    this.state = {
+      loading: true,
+    };
+  }
+
+  handleToggleLoading(bool) {
+    this.setState({
+      loading: bool,
+    });
+  }
+
 
   render() {
-    // const {
-    //   loading,
-    // } = this.state;
 
-    // return (
-    //   <div>
-    //     <HtmlHeaderTags />
-    //     { loading ? this._renderLoading() : this._renderApp(this.state.selecting) }
-    //   </div>
-    // );
     return (
       <div>
         <HtmlHeaderTags />
-        <Canvas />
+        <Game {...this.state} toggleLoading={this.handleToggleLoading.bind(this)} />
       </div>
     );
   }
 }
-
-App.prototype.displayName = 'App';
