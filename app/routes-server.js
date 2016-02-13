724acf16109b60382382222338db9778ef3bc036
keithpops <keith.poplawski@gmail.com>
2016-02-13 01:43:26 -0500

    Revert "removes react router"
    This reverts commit a0598f7a05a6cf2ed3bcf76443ef043d2ec58a2c.

--- app/routes-server.js
+++ app/routes-server.js
@@ -1,6 +1,18 @@
+// LIBRARY
+/*eslint-disable no-unused-vars*/
 import React from 'react';
+/*eslint-enable no-unused-vars*/
+import {Router, Route, IndexRoute} from 'react-router';
 
 // COMPONENT
 import Application from './components/App/App';
 
-export default Application;
+import createLocation from 'history/lib/createLocation';
+
+let history = createLocation();
+
+export default (
+  <Router history={history}>
+    <Route path='/' component={Application} />
+  </Router>
+);
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,12 @@
+// LIBRARY
+/*eslint-disable no-unused-vars*/
 import React from 'react';
+/*eslint-enable no-unused-vars*/
+import {Route, IndexRoute} from 'react-router';
 
 // COMPONENT
 import Application from './components/App/App';
 
-export default Application;
+export default (
+    <Route path='/' component={Application} />
+);
