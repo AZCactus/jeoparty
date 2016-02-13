ff83e3584b70e3a7faa801e2eaf68366962f2209
keithpops <keith.poplawski@gmail.com>
2016-02-12 23:03:36 -0500

    removes unneeded routes

--- app/routes.js
+++ app/routes.js
@@ -6,17 +6,7 @@ import {Route, IndexRoute} from 'react-router';
 
 // COMPONENT
 import Application from './components/App/App';
-import Contact from './components/ContactSection/Contact';
-import HomeSection from './components/HomeSection/HomeSection';
-import NotFoundSection from './components/NotFoundSection/NotFoundSection';
-import TodoSection from './components/TodoSection/TodoSection';
 
 export default (
-    <Route path='/' component={Application}>
-      <IndexRoute component={HomeSection} />
-      <Route path='home' component={HomeSection}/>
-      <Route path='todo' component={TodoSection}/>
-      <Route path='contact' component={Contact}/>
-      <Route path='*' component={NotFoundSection} />
-    </Route>
+    <Route path='/' component={Application} />
 );
