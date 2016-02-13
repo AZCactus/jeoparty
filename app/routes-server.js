a0598f7a05a6cf2ed3bcf76443ef043d2ec58a2c
keithpops <keith.poplawski@gmail.com>
2016-02-12 23:24:00 -0500

    removes react router

--- app/routes-server.js
+++ app/routes-server.js
@@ -1,18 +1,6 @@
-// LIBRARY
-/*eslint-disable no-unused-vars*/
 import React from 'react';
-/*eslint-enable no-unused-vars*/
-import {Router, Route, IndexRoute} from 'react-router';
 
 // COMPONENT
 import Application from './components/App/App';
 
-import createLocation from 'history/lib/createLocation';
-
-let history = createLocation();
-
-export default (
-  <Router history={history}>
-    <Route path='/' component={Application} />
-  </Router>
-);
+export default Application;
--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,6 @@
-// LIBRARY
-/*eslint-disable no-unused-vars*/
 import React from 'react';
-/*eslint-enable no-unused-vars*/
-import {Route, IndexRoute} from 'react-router';
 
 // COMPONENT
 import Application from './components/App/App';
 
-export default (
-    <Route path='/' component={Application} />
-);
+export default Application;
