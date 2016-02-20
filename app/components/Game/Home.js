c762c971ab6e389348773c1890d9f347863de73a
keithpops <keith.poplawski@gmail.com>
2016-02-20 02:15:58 -0500

    update styles for home screen

--- app/components/Game/Home.js
+++ app/components/Game/Home.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 
 import GameActions from '../../actions/GameActions';
 
+import logo from '../../images/jeoparty.jpg';
+
 import VelocityComponent from 'velocity-react/velocity-component';
 
 
@@ -42,12 +44,33 @@ export default class Home extends Component {
   render() {
 
     return (
-      <div className="flex flex-column flex-center flex-middle view">
-        <h1 style={{fontSize: 64, textTransform: "uppercase"}}>Jeoparty!</h1>
-        <h2>It's party time!</h2>
-        <VelocityComponent animation={{opacity: this.state.blink ? 1 : 0}} duration={120}>
-          <h3 style={{color: "gold", textTransform: "uppercase"}}>Press Any Button!</h3>
-        </VelocityComponent>
+      <div className="view" style={{backgroundImage: `url(${logo})`, backgroundSize: 'cover'}}>
+        <h3
+          className="white"
+          style={{
+            textTransform: "uppercase",
+            opacity: this.state.blink ? 0 : 1,
+            position: 'fixed',
+            letterSpacing: 12,
+            top: 50,
+            fontSize: 32,
+            width: '100%',
+            textAlign: 'center'}}>
+          It's Party Time!
+        </h3>
+        <h3
+          className="yellow"
+          style={{
+            textTransform: "uppercase",
+            opacity: this.state.blink ? 1 : 0,
+            position: 'fixed',
+            letterSpacing: 12,
+            bottom: 50,
+            fontSize: 32,
+            width: '100%',
+            textAlign: 'center'}}>
+          Press Any Button
+        </h3>
       </div>
     );
   }
